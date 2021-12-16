34ea91a8b8823dcb41c1a209eb6cdc7cfcb6ab55
Jialun Song <jialun.song@hm.com>
2021-12-16 22:02:45 +0100

    no pandas, not readible, puzzle becomes boring

--- day16.py
+++ day16.py
@@ -6,9 +6,6 @@ from misc import get_input
 
 INPUT_URL = "https://adventofcode.com/2021/day/16/input"
 
-def gt(x): return 1 if x[0] > x[1] else 0
-def lt(x): return 1 if x[0] < x[1] else 0
-def et(x): return 1 if x[0] == x[1] else 0
 def operate(operator, values):
     return [
         sum,
@@ -16,13 +13,13 @@ def operate(operator, values):
         min,
         max,
         print,
-        gt,
-        lt,
-        et
+        lambda x: 1 if x[0] > x[1] else 0,
+        lambda x: 1 if x[0] < x[1] else 0,
+        lambda x: 1 if x[0] == x[1] else 0
     ][operator](values)
     
 
-def decode_packets_num(packets, versions, number, operator=None):
+def decode_packets_num(packets, versions=[], number=1, operator=None):
     p_start, results = 0, []
     for _ in range(number):
         p_version, p_type = int(packets[p_start:p_start+3], 2), int(packets[p_start+3:p_start+6], 2)
@@ -33,17 +30,17 @@ def decode_packets_num(packets, versions, number, operator=None):
             while packets[p_start] == "1":  # not last group
                 value += packets[p_start+1:p_start+5]
                 p_start += 5
-            value += packets[p_start+1:p_start+5]
-            p_start += 5  # last group
+            value += packets[p_start+1:p_start+5]  # last group
+            p_start += 5
             results.append(int(value, 2))
         else:
             if packets[p_start+6] == "0":  # length_type_id 0: 
-                length = int(packets[p_start+7:p_start+22], 2)
+                length = int(packets[p_start+7:p_start+7+15], 2)
                 versions, result = decode_packets_len(packets[p_start+7+15:p_start+7+15+length], versions, length, p_type)
                 p_start += 7 + 15 + length
             else:  # length_type_id 1
-                number = int(packets[p_start+7:p_start+18], 2)
-                versions, p_start_move, result = decode_packets_num(packets[p_start+18:], versions, number, p_type)
+                number = int(packets[p_start+7:p_start+7+11], 2)
+                versions, p_start_move, result = decode_packets_num(packets[p_start+7+11:], versions, number, p_type)
                 p_start += 7 + 11 + p_start_move
             results.append(result)
     
@@ -61,17 +58,17 @@ def decode_packets_len(packets, versions, p_length, operator=None):
             while packets[p_start] == "1":  # not last group
                 value += packets[p_start+1:p_start+5]
                 p_start += 5
-            value += packets[p_start+1:p_start+5]
-            p_start += 5  # last group
+            value += packets[p_start+1:p_start+5]  # last group
+            p_start += 5
             results.append(int(value, 2))
         else:
             if packets[p_start+6] == "0":  # length_type_id 0: 
-                length = int(packets[p_start+7:p_start+22], 2)
+                length = int(packets[p_start+7:p_start+7+15], 2)
                 versions, result = decode_packets_len(packets[p_start+7+15:p_start+7+15+length], versions, length, p_type)
                 p_start += 7 + 15 + length
             else:  # length_type_id 1
-                number = int(packets[p_start+7:p_start+18], 2)
-                versions, p_start_move, result = decode_packets_num(packets[p_start+18:], versions, number, p_type)
+                number = int(packets[p_start+7:p_start+7+11], 2)
+                versions, p_start_move, result = decode_packets_num(packets[p_start+7+11:], versions, number, p_type)
                 p_start += 7 + 11 + p_start_move
             results.append(result)
     
@@ -84,8 +81,7 @@ def puzzle1(answer=None):
         return answer
     raw_input = get_input(INPUT_URL).strip()
     bin_packet = bin(int(raw_input, 16))[2:].zfill(len(raw_input*4))
-    versions = []
-    versions, *_ = decode_packets_num(bin_packet, versions, number=1)
+    versions, *_ = decode_packets_num(bin_packet)
     print(sum(map(int, versions)))
     
 
@@ -94,8 +90,7 @@ def puzzle2(answer=None):
         return answer
     raw_input = get_input(INPUT_URL).strip()
     bin_packet = bin(int(raw_input, 16))[2:].zfill(len(raw_input*4))
-    versions = []
-    versions, _, results = decode_packets_num(bin_packet, versions, number=1)
+    *_, results = decode_packets_num(bin_packet)
     print(results)
 
 
